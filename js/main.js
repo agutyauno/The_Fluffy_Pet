9225b9d17bea0e3be3aa0e15887ec0e6a3329da0
agutyauno <buihoainam2606@gmail.com>
2025-04-06 14:33:15 +0700

    hoàn thành footer và header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 // Cập nhật tỷ lệ khi load và resize
 function updateScaleFactor() {
-    const viewportWidth = window.innerWidth;
-    const scaleFactor = viewportWidth / 1920;
-    document.documentElement.style.setProperty('--scale-factor', scaleFactor);
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth > 1920) {
+    document.documentElement.style.setProperty('--scale-factor', 1);
+    return;
   }
-  
-  window.addEventListener('load', updateScaleFactor); // Khởi chạy lần đầu
-  window.addEventListener('resize', updateScaleFactor); // Cập nhật khi thay đổi kích thước cửa sổ
-  
+  const scaleFactor = viewportWidth / 1920;
+  document.documentElement.style.setProperty('--scale-factor', scaleFactor);
+}
+
+window.addEventListener('load', updateScaleFactor); // Khởi chạy lần đầu
+window.addEventListener('resize', updateScaleFactor); // Cập nhật khi thay đổi kích thước cửa sổ
+
