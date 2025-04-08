26d88d1066610994295aa8251be4f318612ce80d
agutyauno <buihoainam2606@gmail.com>
2025-04-08 20:41:35 +0700

    test

--- js/main.js
+++ js/main.js
@@ -14,30 +14,3 @@ function updateScaleFactor() {
 window.addEventListener('load', updateScaleFactor); // Khởi chạy lần đầu
 window.addEventListener('resize', updateScaleFactor); // Cập nhật khi thay đổi kích thước cửa sổ
 
-// Load header
-fetch('../components/header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
-
-// Load footer
-fetch('../components/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  });
-
-window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.nav_bar');
-  const body = document.body;
-  const scrollPosition = window.scrollY;
-
-  if (scrollPosition > 115 * scaleFactor) { // Thay đổi giá trị này theo nhu cầu
-    navbar.classList.add('fixed');
-    body.classList.add('navbar-fixed');
-  } else {
-    navbar.classList.remove('fixed');
-    body.classList.remove('navbar-fixed');
-  }
-});
